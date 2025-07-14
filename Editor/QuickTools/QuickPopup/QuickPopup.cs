ad8d3e3e1dbdd3abe0087aa2c2a45ad6d307da7a
Bjørnar <bjornar24@gmail.com>
2025-07-14 11:23:01 +0200

    Added simple QuickPopup framework

---
+++ Editor/QuickTools/QuickPopup/QuickPopup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BearsEditorTools.Internal;
+
+namespace BearsEditorTools
+{
+    public static class QuickPopup
+    {
+        public struct WindowParams
+        {
+            public float Width;
+            public float Height;
+            public Action OnGUIHeaderFunction;
+            public Action OnGUIFunction;
+            public Action OnCloseFunction;
+            public bool NeedScroll;
+            
+            public WindowParams(float width, float height, Action onGUIFunction, bool needScroll = false, Action onGUIHeaderFunction = null, Action onCloseFunction = null)
+            {
+                Width = width;
+                Height = height;
+                OnGUIHeaderFunction = onGUIHeaderFunction;
+                OnGUIFunction = onGUIFunction;
+                OnCloseFunction = onCloseFunction;
+                NeedScroll = needScroll;
+            }
+        }
+        
+        public static void ShowWindow(WindowParams windowParams)
+        {
+            QuickPopupBaseWindow.ShowWindow(windowParams);
+        }
+
+        public static void CloseCurrent()
+        {
+            QuickPopupBaseWindow.CloseCurrent();
+        }
+    }
+}
---
+++ Editor/QuickTools/QuickPopup/QuickPopupBaseWindow.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace BearsEditorTools.Internal
+{
+    internal class QuickPopupBaseWindow : EditorWindow
+    {
+        public QuickPopup.WindowParams windowParams;
+
+        private static QuickPopupBaseWindow _currentWindow;
+
+        public static void ShowWindow(QuickPopup.WindowParams windowParams) 
+        {
+            QuickPopupBaseWindow window = ScriptableObject.CreateInstance<QuickPopupBaseWindow>();
+            
+            _currentWindow = window;
+            
+            window.windowParams = windowParams;
+            
+            window.ShowPopup();
+            window.Focus();
+
+            Rect windowRect = window.position;
+
+            if (Event.current != null)
+            {
+                var mousePosition = GUIUtility.GUIToScreenPoint(Event.current.mousePosition);
+                windowRect.x = mousePosition.x - windowRect.width * 0.5f;
+                windowRect.y = mousePosition.y;
+            }
+            else
+            {
+                windowRect.x = Screen.width * 0.5f - windowRect.width * 0.5f;
+                windowRect.y = Screen.height * 0.5f - windowRect.height * 0.5f;
+            }
+
+            windowRect.width = windowParams.Width;
+            windowRect.height = windowParams.Height;
+            window.position = windowRect;
+        }
+
+        private Vector2 scrollPosition;
+        void OnGUI()
+        {
+            windowParams.OnGUIHeaderFunction?.Invoke();
+
+            if (windowParams.NeedScroll)
+            {
+                scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+            }
+            
+            windowParams.OnGUIFunction?.Invoke();
+            
+            if (windowParams.NeedScroll)
+            {
+                GUILayout.EndScrollView();
+            }
+            
+            if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Escape)
+            {
+                windowParams.OnCloseFunction?.Invoke();
+                Close();
+            }
+
+            
+            GUI.backgroundColor = Color.red;
+            
+            if (GUILayout.Button("Close"))
+            {
+                windowParams.OnCloseFunction?.Invoke();
+                Close();
+            }
+
+            GUI.backgroundColor = Color.white;
+            
+            // If not focused, close the window
+            if (EditorWindow.focusedWindow != this || windowParams.OnGUIFunction == null)
+            {
+                Close();
+                _currentWindow = null;
+            }
+        }
+        
+        /*void OnInspectorUpdate()
+        {
+            Repaint();
+        }*/
+        public static void CloseCurrent()
+        {
+            if (_currentWindow)
+            {
+                _currentWindow.windowParams.OnCloseFunction?.Invoke();
+                _currentWindow.Close();
+                _currentWindow = null;
+            }
+        }
+    }
+}
